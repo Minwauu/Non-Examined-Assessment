83d790eafde5ad53968aae33d8d836c67bb59ed4
Mena <menaabdalla28@gmail.com>
2025-03-10 15:11:24 +0000

    Auto commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 from flask import Flask, redirect, url_for, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required, current_user
+#quick commitS
