401d78e3143d36343733d3847c65eb840de5611e
Mena <menaabdalla28@gmail.com>
2025-03-11 18:58:20 +0000

    Auto commit

--- app.py
+++ app.py
@@ -9,3 +9,12 @@ app.config['SQLALCHEMY_DATABSE'] = 'sqlite:///cinema_booking_system.db'
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique = True, nullable = False)
+    email = db.Column(db.String(100), unique=True, nullable = False)
+    password = db.Column(db.String(100), nullable = False)
