f024657eaa26b63b9b57a7237a7611323f869dbb
Mena <menaabdalla28@gmail.com>
2025-03-12 18:32:51 +0000

    Auto commit

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required, current_user
 from flask_bcrypt import Bcrypt
 
+#flask setup
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'minwauu'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cinema_booking_system.db'
