96d40f0feb61268c5f659bedfd3827768efd8a82
Mena <menaabdalla28@gmail.com>
2025-03-14 20:05:53 +0000

    Auto commit

--- models.py
+++ models.py
@@ -1,9 +1,9 @@
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, Enum
 from flask_login import UserMixin
 from extensions import db
 from datetime import datetime
 
-# user model (admin + customers for now)
+# user model (admin + customers for now maybe add accessability users)
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique = True, nullable = False)
@@ -17,7 +17,7 @@ class Movie(db.Model):
     title = db.Column(db.String(100), unique = True, nullable =False)
     duration = db.Column(db.Integer, nullable = False)
     description = db.Column(db.Text, nullable=False)
-    screenings = db.relationship('Screening', backref = 'movie' lazy = True)
+    screenings = db.relationship('Screening', backref = 'movie', lazy = True)
 
 # screenings 
 
@@ -34,11 +34,28 @@ class Showtime(db.Model):
     start_time = db.Column(db.DateTime, nullable=False)
     screening_id = db.Column(db.Integer, db.ForeignKey('screening.id'), nullable = False) #will be used in bookings
 
+#bookings and status
+
+class SeatStatus(Enum):
+    AVAILABLE = "available"
+    RESERVED = "reserved"
+    BOOKED = "booked"
+    ACCESSIBLE_AVAILABLE = "accessible_available"
+    ACCESSIBLE_RESERVED = "accessible_reserved"
+    ACCESSIBLE_BOOKED = "accessible_booked"
+    UNAVAILABLE = "unavailable"
+
+
 class SeatBooking(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    seat_number = db.Column(db.String(100), nullable = False)
-    availability = db.Column(db.String(100), nullable = False)
+    seat_number = db.Column(db.String(10), nullable = False)
+    status = db.Column(db.Boolean, default = True, nullable = False)
     screening_id = db.Column(db.Integer, db.ForeignKey('screening.id'), nullable = False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+    accessability = db.Column(db.Boolean, default = True, nullable=False)
+    screening = db.relationship("Screening", backref = "seatbooking")
+    user = db.relationship("User", backref = "bookings") 
+
+
 
 
